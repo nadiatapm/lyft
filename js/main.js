c7693bb2ce55699e7a713480cd0cc72f70546e59
nadia tapia <nadiatap@gmail.com>
2016-11-28 14:20:00 -0300

    arreglo img bandera

--- js/main.js
+++ js/main.js
@@ -1,15 +1,63 @@
-/*function validateForm(){
-	function validarNombre(){
-		var nombre = $("#nameUser").val();
-		if(nombre == null || nombre.length == 0 ){
-			$(".form-group").append("<span>Ingrese Nombre</span>");
-			return false;
-		}else if(!(nombre[0] == nombre[0].toUpperCase())){
-			$(".form-group").append("<span>Nombre en Mayuscula</span>");
-			return false;
-		}else if(/[0-9]/.test(nombre)){
-			$("form-group-").append("<span>formato no valido</span>");
-			return false;
-		}
-	}validarNombre();
-}*/
+
+var paises = [
+{
+	nombre: "Chile",
+	prefijo: "+569",
+	bandera: "img/CL.png"
+},
+{
+	nombre: "Peru",
+	prefijo: "+519",
+	bandera: "img/PE.png"
+},
+{
+	nombre: "mexico",
+	prefijo: "+529",
+	bandera: "img/MX.png"
+},
+{
+	nombre: "Venezuela",
+	prefijo: "+58",
+	bandera: "img/VN.png"
+},
+{
+	nombre: "Estados Unidos",
+	prefijo: "+1",
+	bandera: "img/US.png"
+},
+{
+	nombre: "Reino Unido",
+	prefijo: "+44",
+	bandera: "img/GB.png"
+},
+{
+	nombre: "Japon",
+	prefijo: "+81",
+	bandera: "img/JP.png"
+},
+{
+	nombre: "Brasil",
+	prefijo: "+55",
+	bandera: "img/BR.png"
+},
+{
+	nombre: "Francia",
+	prefijo: "+33",
+	bandera: "img/FR.png"
+}
+];
+
+// traerme el nombre del pais desde localStorage
+var nombre_pais = window.localStorage.getItem("nombre_pais") || "chile";
+
+// recuperar el objeto pais, desde el arreglo de paises
+var pais = {};
+for (var i=0 ; i<paises.length ; ++i){
+	if (paises[i].nombre == nombre_pais) {
+		pais = paises[i];
+		break;
+	}
+}
+// solo queda modificar el DOM
+$('#pais img').attr('src', pais.bandera);
+$('#prefijo').html(pais.prefijo);
--- js/paises.js
+++ js/paises.js
@@ -1,68 +1,7 @@
+$('#paises a').on('click', function(e){
+	// recupero el nombre del pais pulsado
+	var pais_pulsado = $(this).find('span').html();
 
-var paises = [
-{
-	nombre: "chile",
-	prefijo: "+569",
-	bandera: "img/Bandera-chilena.png"
-},
-{
-	nombre: "peru",
-	prefijo: "+519",
-	bandera: "img/Bandera_Perú.png"
-},
-{
-	nombre: "mexico",
-	prefijo: "+529",
-	bandera: "img/05-bandera-federal-historia-1823-1864-bandera-mexico.png"
-},
-{
-	nombre: "venezuela",
-	prefijo: "+58",
-	bandera: "img/resize-img.png"
-},
-{
-	nombre: "estados unidos",
-	prefijo: "+1",
-	bandera: "img/pegatinas-coches-motos-usa.png"
-},
-{
-	nombre: "Reino Unido",
-	prefijo: "+44",
-	bandera: "img/INGLESA-500x500.png"
-},
-{
-	nombre: "Japon",
-	prefijo: "+81",
-	bandera: "img/Bandera_de_Japon.png"
-},
-{
-	nombre: "Brasil",
-	prefijo: "+55",
-	bandera: "img/Brasil-bandera-1.png"
-},
-{
-	nombre: "Alemania",
-	prefijo: "+49",
-	bandera: "img/bandera-pais-alemania-500x500.png"
-},
-{
-	nombre: "Francia",
-	prefijo: "+33",
-	bandera: "img/francia-500x500.png"
-}
-];
-
-// traerme el nombre del pais desde localStorage
-var nombre_pais = window.localStorage.getItem("nombre_pais") || "chile";
-
-// recuperar el objeto pais, desde el arreglo de paises
-var pais = {};
-for (var i=0 ; i<paises.length ; ++i){
-	if (paises[i].nombre == nombre_pais) {
-		pais = paises[i];
-		break;
-	}
-}
-// solo queda modificar el DOM
-$('#pais img').attr('src', pais.bandera);
-$('#prefijo').html(pais.prefijo);
+	// lo guardo en localStorage
+	window.localStorage.setItem('nombre_pais', pais_pulsado);
+});
